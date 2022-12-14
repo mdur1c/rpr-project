b2b43c6f4435cd1dba218f296b91c0f9f0fd5ce9
mdur1c <mduric1@etf.unsa.ba>
2022-12-14 02:10:23 +0100

    1.4 user domain

--- src/main/java/ba/unsa/etf/rpr/domain/user.java
+++ src/main/java/ba/unsa/etf/rpr/domain/user.java
@@ -4,8 +4,57 @@ public class user {
     private int user_id;
     private String username,password,email;
 
-    public int getId(){
+    public int getUser_id(){
         return user_id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof user)) return false;
+        user user = (user) o;
+        return getUser_id() == user.getUser_id() && getUsername().equals(user.getUsername()) && getPassword().equals(user.getPassword()) && getEmail().equals(user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUser_id(), getUsername(), getPassword(), getEmail());
+    }
 }
